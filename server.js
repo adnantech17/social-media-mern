9411811a82459b075cbb0f9edaa07ca0bbe58a5b
MD. ADNAN ALI <adnantech17@gmail.com>
2021-12-09 21:45:52 +0600

    Heroku Commit 3333

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    console.log("Hello");
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
